1d7537c8435927a39dc98f9c463d917d5d9b78a0
HariLionelMessi <harigarethbale132@gmail.com>
2024-08-28 20:32:50 +0530

    kinda small edit to checking

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@
 
 const Dashboard = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen">Erlvomr yo Dashboard</div>
+        <div className="flex justify-center items-center min-h-screen">Dashboard Brother</div>
     )
 }
 
